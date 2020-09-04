7a1e3d60730b0387b67fe117fd4b7070e2a61ba3
James Jenner <james.jenner@deswik.com>
2020-09-05 09:16:34 +1000

    Initial version

---
+++ app/ContextPadProvider.js
@@ -0,0 +1,50 @@
+/**
+ * Context pad provider.
+ */
+export default function ContextPadProvider(connect, contextPad, modeling) {
+  this._connect = connect;
+  this._modeling = modeling;
+
+  contextPad.registerProvider(this);
+}
+
+ContextPadProvider.$inject = [
+  'connect',
+  'contextPad',
+  'modeling'
+];
+
+
+ContextPadProvider.prototype.getContextPadEntries = function(element) {
+  var connect = this._connect,
+      modeling = this._modeling;
+
+  function removeElement() {
+    modeling.removeElements([ element ]);
+  }
+
+  function startConnect(event, element, autoActivate) {
+    connect.start(event, element, autoActivate);
+  }
+
+  return {
+    'delete': {
+      group: 'edit',
+      className: 'context-pad-icon-remove',
+      title: 'Remove',
+      action: {
+        click: removeElement,
+        dragstart: removeElement
+      }
+    },
+    'connect': {
+      group: 'edit',
+      className: 'context-pad-icon-connect',
+      title: 'Connect',
+      action: {
+        click: startConnect,
+        dragstart: startConnect
+      }
+    }
+  };
+};
